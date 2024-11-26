09402027821083b355009447163d856e1073eeec
Isabella Bicudo de Souza <isabellabicudo@hotmail.com>
2024-11-26 11:25:49 -0300

    1016

---
+++ Java/Beginner/Distance.java
@@ -0,0 +1,16 @@
+package Java.Beginner;
+
+import java.util.Scanner;
+
+//1016 - Distance
+public class Distance {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int distance = input.nextInt();
+
+        System.out.println(distance * 2 + " minutos");
+        input.close();
+
+        
+    }
+}
